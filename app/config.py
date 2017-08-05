2ae4414f1d886d06d5adf7312a45f861f13f7a7b
Carson <carsnwd@gmail.com>
2017-08-05 13:32:23 -0400

    Config.py Added

---
+++ app/config.py
@@ -0,0 +1,10 @@
+WTF_CSRF_ENABLED = True
+SECRET_KEY = '~H;+=2NN8%wc+A2Hr,6bh*N96&@u<uH%'
+DB_USERNAME = 'postgres'
+DB_PASSWORD = 'q!?8BY?%sQv=z$X#qfMt&M8BZ?cUMW8t'
+DB_HOST = 'localhost'
+DB_PORT = '5432'
+DB_NAME = 'shiplist'
+DB_URI = 'postgres://' + DB_USERNAME + ":" + DB_PASSWORD + "@" + DB_HOST + ":" + DB_PORT + "/" + DB_NAME
+RECAPTCHA_PUBLIC_KEY = '6LciDA0UAAAAALhG4OFvzE-l3zwDpGddPuw56jAU'
+RECAPTCHA_PRIVATE_KEY = '6LciDA0UAAAAAKpSdin8IJP9tjjH7nAtjtQf9vyO'
